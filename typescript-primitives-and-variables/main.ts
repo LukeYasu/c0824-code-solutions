d226db6e09682b8f2fcf4177fc39470ec4ecaa4f
LukeYasu <lucasyasumura@gmail.com>
2024-08-26 21:52:06 +0000

    completed typescript-primitives-and-variables exersize

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,16 @@
+const fullName: string = 'Lucas Yasumura';
+const isCool: boolean = true;
+const totalPets: number = 1;
+const empty: null = null;
+const nothing: undefined = undefined;
+
+console.log('Full Name: ', fullName);
+console.log('Is Cool: ', isCool);
+console.log('Total Pets: ', totalPets);
+console.log('typeof Full Name: ', typeof fullName);
+console.log('typeof Is Cool: ', typeof isCool);
+console.log('typeof Total Pets: ', typeof totalPets);
+console.log('Empty: ', empty);
+console.log('typeof Empty: ', typeof empty);
+console.log('Nothing: ', nothing);
+console.log('typeof Nothing: ', nothing);
