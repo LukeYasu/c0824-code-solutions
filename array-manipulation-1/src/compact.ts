645089e5d78637d3367b3fe8513e96c83e2a5672
LukeYasu <lucasyasumura@gmail.com>
2024-09-03 16:37:15 +0000

    completed array-manipulation-1 exercise

--- array-manipulation-1/src/compact.ts
+++ array-manipulation-1/src/compact.ts
@@ -2,19 +2,7 @@
 function compact(array: unknown[]): unknown[] {
   const compactArr = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] !== false) {
-      if (array[i] !== null) {
-        if (!Number.isNaN(array[i])) {
-          if (array[i] !== 0) {
-            if (array[i] !== '') {
-              if (array[i] !== undefined) {
-                compactArr.push(array[i]);
-              }
-            }
-          }
-        }
-      }
-    }
+    if (array[i]) compactArr.push(array[i]);
   }
   return compactArr;
 }
