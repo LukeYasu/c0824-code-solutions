d60298027dae5016b90e078eacdbcc71064eb877
LukeYasu <lucasyasumura@gmail.com>
2024-09-10 16:46:33 +0000

    completed dom-querying exercise

---
+++ dom-querying/main.ts
@@ -0,0 +1,11 @@
+console.log('hello, world');
+const $h1 = document.querySelector('.h1');
+console.dir($h1);
+const $element = document.getElementById('explanation');
+console.dir($element);
+const $class = document.getElementsByClassName('hint');
+console.dir($class);
+const $pNodes = document.querySelectorAll('p');
+console.log($pNodes);
+const $exampleLinksNodes = document.querySelectorAll('.example-link');
+console.log($exampleLinksNodes);
