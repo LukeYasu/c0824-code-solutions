a8dcfa5adac2fc1deeb0f385332fb4c721658bf3
LukeYasu <lucasyasumura@gmail.com>
2024-09-10 19:26:50 +0000

    completed dom-events exercise

--- dom-events/buttons.ts
+++ dom-events/buttons.ts
@@ -1,6 +1,6 @@
 const $clickButton = document.querySelector('.click-button');
 if (!$clickButton) throw new Error('$clickButton does not exist');
-function handleClick(event: Event): any {
+function handleClick(event: Event): void {
   console.log('button clicked');
   console.log('event', event);
   console.log('event.target', event.target);
@@ -10,7 +10,7 @@ $clickButton.addEventListener('click', handleClick);
 
 const $hoverButton = document.querySelector('.hover-button');
 if (!$hoverButton) throw new Error('$hoverButton does not exist');
-function handleMouseover(event: Event): any {
+function handleMouseover(event: Event): void {
   console.log('button hovered');
   console.log('event', event);
   console.log('event.target', event.target);
@@ -20,7 +20,7 @@ $hoverButton.addEventListener('mouseover', handleMouseover);
 
 const $doubleClickButton = document.querySelector('.double-click-button');
 if (!$doubleClickButton) throw new Error('$doubleClickButton does not exist');
-function handleDoubleClick(event: Event): any {
+function handleDoubleClick(event: Event): void {
   console.log('button double clicked');
   console.log('event', event);
   console.log('event.target', event.target);
