e0f06afe66609e81667c4eccf4203cc83fcf23c1
LukeYasu <lucasyasumura@gmail.com>
2024-09-12 17:30:30 +0000

    completed typescript-light-bulb challenge

---
+++ typescript-light-bulb/main.ts
@@ -0,0 +1,26 @@
+const $button = document.querySelector('button');
+const $body = document.querySelector('body');
+if (!$button) throw new Error('the !$button query failed');
+if (!$body) throw new Error('the !$body query failed');
+
+$body.addEventListener('click', handleClick);
+
+function handleClick(event: Event): void {
+  const eventTarget = event.target as HTMLInputElement;
+  const eventTargetBackground = eventTarget.closest('body') as HTMLBodyElement;
+  console.log('eventTarget: ', eventTarget);
+  console.log('eventTarget.tagName: ', eventTarget.tagName);
+  if (eventTarget.tagName !== 'BUTTON') {
+    return;
+  }
+
+  console.log('eventTarget.closest: ', eventTarget.closest('body'));
+
+  if (eventTarget.className === 'light-mode') {
+    eventTarget.className = 'dark-mode';
+    eventTargetBackground.className = 'background-dark';
+  } else if (eventTarget.className === 'dark-mode') {
+    eventTarget.className = 'light-mode';
+    eventTargetBackground.className = 'background-light';
+  }
+}
