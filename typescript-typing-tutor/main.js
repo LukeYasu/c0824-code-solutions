4a733467cd834c6f93e3cf101e33a186612bdfbf
LukeYasu <lucasyasumura@gmail.com>
2024-09-13 04:19:30 +0000

    completed typescript-typing-tutor challenge

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,31 +1,51 @@
 'use strict';
-// const $textChallenge = document.querySelector('.text-challenge');
-// if (!$textChallenge) throw new Error('The $textChallenge query failed');
-// const $body = document.querySelector('body');
-// if (!$body) throw new Error('The $body query failed');
-// const $span = document.querySelectorAll('span');
-// if (!$span) throw new Error('The $span query failed');
-// let $spanLength = $span.length;
-// let i = 0;
-// function handleKeydown(event: KeyboardEvent): void {
-//   const eventTarget = event.target as HTMLElement;
-//   const eventKey = event.key;
-//   console.log('eventKey: ', eventKey);
-//   const currentClassList = $span[i].classList;
-//   // if (currentClassList.contains('space') && eventKey === ' ') {
-//   //   $span[i].className = 'typed-correct';
-//   //   i++;
-//   //   $span[i].className = 'upcoming-character space';
-//   // }
-//   if (eventKey === $span[i].textContent) {
-//     $span[i].className = 'typed-correct';
-//     i++;
-//     $span[i].className = 'upcoming-character';
-//   } else if (currentClassList.contains('space') && eventKey === ' ') {
-//     $span[i].className = 'typed-correct';
-//     i++;
-//   } else {
-//     $span[i].className = 'typed-incorrect upcoming-character';
-//   }
-// }
-// $body.addEventListener('keydown', handleKeydown);
+const $body = document.querySelector('body');
+if (!$body) throw new Error('The $body query failed');
+const $span = document.querySelectorAll('span');
+if (!$span) throw new Error('The $span query failed');
+const $divAcc = document.createElement('div');
+$divAcc.className = 'accuracy';
+let i = 0;
+let errorCount = 0;
+let correctCount = 0;
+function handleKeydown(event) {
+  const eventTarget = event.target;
+  const eventKey = event.key;
+  console.log('eventKey: ', eventKey);
+  console.log('eventTarget', eventTarget);
+  let currentClassList = $span[i].classList;
+  if (eventKey === $span[i].textContent) {
+    $span[i].className = 'typed-correct';
+    i++;
+    correctCount += 1;
+    currentClassList = $span[i].classList;
+    if (currentClassList.contains('space')) {
+      $span[i].className = 'space upcoming-character';
+    } else {
+      $span[i].className = 'upcoming-character';
+    }
+  } else if (currentClassList.contains('space') && eventKey === ' ') {
+    $span[i].className = 'typed-correct';
+    i++;
+    correctCount += 1;
+    currentClassList = $span[i].classList;
+    if (currentClassList.contains('space')) {
+      $span[i].className = 'space upcoming-character';
+    } else {
+      $span[i].className = 'upcoming-character';
+    }
+  } else {
+    errorCount += 1;
+    currentClassList = $span[i].classList;
+    if (currentClassList.contains('space')) {
+      $span[i].className = 'space upcoming-character typed-incorrect';
+    } else {
+      $span[i].className = 'upcoming-character typed-incorrect';
+    }
+  }
+  const accuracy = (1 - errorCount / correctCount) * 100;
+  console.log('accuracy %', accuracy);
+  $divAcc.textContent = `Accuracy: % ${accuracy.toFixed(2)}`;
+  document.body.append($divAcc);
+}
+$body.addEventListener('keydown', handleKeydown);
