86993bdee5393c549c0d00c07e43ce9649cd853f
LukeYasu <lucasyasumura@gmail.com>
2024-09-25 03:24:40 +0000

    completed typescript-carousel challenge 2

--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -13,6 +13,7 @@ const $img5 = document.querySelector('.img5') as HTMLElement;
 
 if (!$currentImage) throw new Error('$currentImage query failed');
 if (!$carousel) throw new Error('$carousel query failed');
+if (!$selectCircles) throw new Error('$selectCircles query failed');
 if (!$img1) throw new Error('$img1 query failed');
 if (!$img2) throw new Error('$img2 query failed');
 if (!$img3) throw new Error('$img3 query failed');
@@ -27,85 +28,80 @@ const images = [
   'images/039.png',
 ];
 const imgDots = [$img1, $img2, $img3, $img4, $img5];
-
 let i = 0;
+let setIntervalId = setInterval(cycleImages, 3000);
+
 function cycleImages(): void {
   i++;
-  for (let i = 0; i < imgDots.length; i++) {
-    imgDots[i].setAttribute('class', `fa-regular fa-circle img${i + 1}`);
-  }
-
+  handleDots();
   if (i === 5) {
     i = 0;
   }
   imgDots[i].setAttribute('class', `fa-sharp fa-solid fa-circle img${i + 1}`);
   $currentImage.setAttribute('src', images[i]);
-
-  console.log('current img: ', images[i]);
 }
 
-let setIntervalId = setInterval(cycleImages, 3000);
+function handleDots(): void {
+  for (let i = 0; i < imgDots.length; i++) {
+    imgDots[i].setAttribute('class', `fa-regular fa-circle img${i + 1}`);
+  }
+}
+
+function dotImageSwitch(num: number): void {
+  $currentImage.setAttribute('src', images[num]);
+  imgDots[num].setAttribute(
+    'class',
+    `fa-sharp fa-solid fa-circle img${num + 1}`
+  );
+  i = num;
+}
 
 $carousel.addEventListener('click', (event: Event) => {
   const eventTarget = event.target as HTMLElement;
-
   clearInterval(setIntervalId);
+
   if (eventTarget.matches('.previous-image-button')) {
-    for (let i = 0; i < imgDots.length; i++) {
-      imgDots[i].setAttribute('class', `fa-regular fa-circle img${i + 1}`);
-    }
+    handleDots();
     i -= 1;
     if (i <= -1) {
       i = 4;
     }
-    imgDots[i].setAttribute('class', `fa-sharp fa-solid fa-circle img${i - i}`);
+    imgDots[i].setAttribute('class', `fa-sharp fa-solid fa-circle img${i}`);
     $currentImage.setAttribute('src', images[i]);
   } else if (eventTarget.matches('.next-image-button')) {
-    for (let i = 0; i < imgDots.length; i++) {
-      imgDots[i].setAttribute('class', `fa-regular fa-circle img${i + 1}`);
-    }
-
+    handleDots();
     i += 1;
     if (i >= images.length) {
       i = 0;
     }
     imgDots[i].setAttribute('class', `fa-sharp fa-solid fa-circle img${i}`);
-
     $currentImage.setAttribute('src', images[i]);
   }
   setIntervalId = setInterval(cycleImages, 3000);
-  console.log(eventTarget);
 });
 
 $selectCircles.addEventListener('click', (event: Event) => {
   const eventTarget = event.target as HTMLElement;
   clearInterval(setIntervalId);
   if (eventTarget.matches('.fa-circle')) {
-    for (let i = 0; i < imgDots.length; i++) {
-      imgDots[i].setAttribute('class', `fa-regular fa-circle img${i + 1}`);
-    }
+    handleDots();
   }
-  if (eventTarget.matches('.img1')) {
-    $currentImage.setAttribute('src', images[0]);
-    $img1.setAttribute('class', 'fa-sharp fa-solid fa-circle img1');
-    i = 0;
-  } else if (eventTarget.matches('.img2')) {
-    $currentImage.setAttribute('src', images[1]);
-    $img2.setAttribute('class', 'fa-sharp fa-solid fa-circle img2');
-    i = 1;
-  } else if (eventTarget.matches('.img3')) {
-    $currentImage.setAttribute('src', images[2]);
-    $img3.setAttribute('class', 'fa-sharp fa-solid fa-circle img3');
-    i = 2;
-  } else if (eventTarget.matches('.img4')) {
-    $currentImage.setAttribute('src', images[3]);
-    $img4.setAttribute('class', 'fa-sharp fa-solid fa-circle img4');
-    i = 3;
-  } else if (eventTarget.matches('.img5')) {
-    $currentImage.setAttribute('src', images[4]);
-    $img5.setAttribute('class', 'fa-sharp fa-solid fa-circle img5');
-    i = 4;
+  switch (true) {
+    case eventTarget.matches('.img1'):
+      dotImageSwitch(0);
+      break;
+    case eventTarget.matches('.img2'):
+      dotImageSwitch(1);
+      break;
+    case eventTarget.matches('.img3'):
+      dotImageSwitch(2);
+      break;
+    case eventTarget.matches('.img4'):
+      dotImageSwitch(3);
+      break;
+    case eventTarget.matches('.img5'):
+      dotImageSwitch(4);
   }
+
   setIntervalId = setInterval(cycleImages, 3000);
-  console.log(eventTarget);
 });
