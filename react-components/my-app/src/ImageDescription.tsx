8066c484bfafa1be8d905d6f79f02ba552b9feee
LukeYasu <lucasyasumura@gmail.com>
2024-10-15 16:20:32 +0000

    completed exercise

--- react-components/my-app/src/ImageDescription.tsx
+++ react-components/my-app/src/ImageDescription.tsx
@@ -1,8 +1,8 @@
 export function ImageDescription() {
   return (
-    <div style={{ fontFamily: 'lorem-ipsum' }}>
+    <div style={{ fontFamily: 'sans-serif', lineHeight: '40px' }}>
       Rayquaza is said to have lived for hundreds of millionsof years. Legends
-      remain of how it put to restthe clash between Kyogre and Groudon.
+      remain of how it put to rest the clash between Kyogre and Groudon.
     </div>
   );
 }
