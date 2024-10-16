ab7ebb2025433dd9ad73f3a0f4f843010bb88d81
LukeYasu <lucasyasumura@gmail.com>
2024-10-16 20:52:20 +0000

    completed with revisions

--- react-toggle-switch/my-app/src/ToggleSwitch.tsx
+++ react-toggle-switch/my-app/src/ToggleSwitch.tsx
@@ -2,20 +2,23 @@ import { useState } from 'react';
 import './App.css';
 
 export function ToggleSwitch() {
-  const [toggle, setToggle] = useState('off');
-
+  const [toggle, setToggle] = useState(true);
+  let onOff = '';
+  if (toggle) {
+    onOff = 'on';
+  } else {
+    onOff = 'off';
+  }
   return (
-    <>
-      <div className="container">
-        <div className={toggle}>
-          <div
-            onClick={() => {
-              toggle === 'off' ? setToggle('on') : setToggle('off');
-            }}
-            className="switch"></div>
-        </div>
-        <div className="on-off-text">{toggle}</div>
+    <div className="container">
+      <div className={onOff}>
+        <div
+          onClick={() => {
+            toggle === true ? setToggle(false) : setToggle(true);
+          }}
+          className="switch"></div>
       </div>
-    </>
+      <div className="on-off-text">{onOff}</div>
+    </div>
   );
 }
