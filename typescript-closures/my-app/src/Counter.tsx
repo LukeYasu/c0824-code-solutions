9bfd8064c3e112f738427e7e89639f71b6d7fe6c
LukeYasu <lucasyasumura@gmail.com>
2024-10-22 01:17:02 +0000

    completed with changes

--- typescript-closures/my-app/src/Counter.tsx
+++ typescript-closures/my-app/src/Counter.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 type Data = {
   foo: string;
@@ -7,12 +7,13 @@ type Data = {
 export function Counter() {
   const [data, setData] = useState<Data>();
   const [counter, setCounter] = useState(0);
+  const callback = useCallback(getData, []);
 
   useEffect(() => {
     const data = getData();
     setData(data);
     setCounter((prev) => prev + 1);
-  }, []);
+  }, [callback]);
 
   function getData() {
     // fetch data
