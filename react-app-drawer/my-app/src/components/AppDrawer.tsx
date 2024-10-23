e1e01620b957b0964adc398a5c1a37a7ee85521f
LukeYasu <lucasyasumura@gmail.com>
2024-10-23 23:43:39 +0000

    completed react-app-drawer challenge

---
+++ react-app-drawer/my-app/src/components/AppDrawer.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+export type MenuItem = {
+  name: string;
+  iconUrl: string;
+  path: string;
+};
+
+type Props = {
+  menuItems: MenuItem[];
+};
+export function AppDrawer({ menuItems }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+  let navWidth = 'navbar-open';
+  if (isOpen) {
+    navWidth = 'navbar-open';
+  } else {
+    navWidth = 'navbar-close';
+  }
+  function handleDrawer() {
+    if (isOpen) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  }
+  return (
+    <div className="flex">
+      <div className="flex">
+        <nav
+          className={`inline-block py-2 px-4 bg-gray-900 navbar ${navWidth}`}>
+          <ul className="flex flex-col">
+            <img
+              onClick={handleDrawer}
+              className="w-10 invert self-start m-3 burger"
+              src="https://www.svgrepo.com/show/509382/menu.svg"
+            />
+            <h1>{isOpen && 'Lucas Shopping'}</h1>
+            {menuItems.map((menu) => (
+              <li key={menu.name} className="inline-block py-2 px-4">
+                <Link to={menu.path} className="text-white flex items-center">
+                  <img className="w-10 invert" src={menu.iconUrl} />
+                  {isOpen && menu.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className="grow ">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
